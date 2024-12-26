a1579d5bce77bf96aed5e7afc5429be853126a85
androy <androyt1@gmail.com>
2024-12-26 19:58:28 +0000

    wrapped with theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/upload" element={<UploadPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
