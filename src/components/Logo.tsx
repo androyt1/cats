162ccd2d8dae395bf420b8630c4d2b0c64332ab2
androy <androyt1@gmail.com>
2024-12-26 20:43:48 +0000

    changed logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,13 +2,20 @@ const Logo = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-8 h-8 text-white"
-      aria-label="Cat Logo"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-paw-print text-stone-400"
     >
-      <path d="M12 2C9.243 2 7 4.243 7 7c0 2.757 2.243 5 5 5s5-2.243 5-5c0-2.757-2.243-5-5-5zm0 8c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3-1.346 3-3 3z" />
-      <path d="M21 22h-2v-5c0-2.757-2.243-5-5-5H10c-2.757 0-5 2.243-5 5v5H3v-5c0-3.309 2.691-6 6-6h4c3.309 0 6 2.691 6 6v5z" />
+      <circle cx="11" cy="4" r="2" color="red" />
+      <circle cx="18" cy="8" r="2" color="green" />
+      <circle cx="20" cy="16" r="2" color="white" />
+      <path d="M9 10a5 5 0 0 1 5 5v3.5a3.5 3.5 0 0 1-6.84 1.045Q6.52 17.48 4.46 16.84A3.5 3.5 0 0 1 5.5 10Z" />
     </svg>
   );
 };
